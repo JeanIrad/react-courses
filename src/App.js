2f1ad2ded5af981c933e46aca88ea65a74685066
Iradukunda Jean de Dieu <85224006+JeanIrad@users.noreply.github.com>
2023-10-03 14:04:16 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import "./styles.css";
+import "./index.css";
 
-export default function App() {
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+function App() {
+  
+  const step = 2;
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step === 1 ? "active" : ""}`}>1</div>
+        <div className={`${step === 2 ? "active" : ""}`}>2</div>
+        <div className={`${step === 3 ? "active" : ""}`}>3</div>
+      </div>
+      <p className="message">
+        step {step}: {messages[step - 1]}
+      </p>
+      <div className="buttons">
+        <button
+          style={{
+            "background-color": "#7950f2",
+            color: "#fff",
+          }}
+        >
+          Previous
+        </button>
+        <button
+          style={{
+            "background-color": "#7950f2",
+            color: "#fff",
+          }}
+        >
+          next
+        </button>
+      </div>
     </div>
   );
 }
+export default App;
